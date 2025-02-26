38240236377a3d4adbe5bc93af6c69892201a7ac
Joel Wembo <joelwembo@outlook.com>
2025-02-27 00:31:04 +0800

    test update

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def index():
         <title>Welcome to prodxcloud</title>
     </head>
     <body>
-        <h1>Hello, Joel Wembo</h1>
-        <p>Welcome to the simple Python web server.</p>
+        <h1>Testing changes</h1>
+        <p>Welcome to the simple Python web server 2.0 .</p>
     </body>
     </html>
     '''
